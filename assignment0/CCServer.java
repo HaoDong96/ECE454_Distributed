0700410bffc1d9b99a850750edc8b5ddbe92ec75
hdong <spdonghao@gmail.com>
2019-05-23 10:48:22 -0400

    parse input

--- assignment0/CCServer.java
+++ assignment0/CCServer.java
@@ -24,13 +24,34 @@ class CCServer {
 			- the payload is a string
 				(UTF-8, big-endian)
 			*/
+			// accept connection from server socket
 			Socket acceptSock = ssock.accept();
 			DataInputStream din = new DataInputStream(acceptSock.getInputStream());
 			int acceptDataLen = din.readInt();
 			byte[] bytes = new byte[acceptDataLen];
 			din.readFully(bytes);
-			String output = new String(bytes, StandardCharsets.UTF_8);
-			System.out.println(din);
+			String input = new String(bytes, StandardCharsets.UTF_8);
+
+			//parse the input numbers
+			// List<int[]> l = Arrays.stream(input.split("\n"))
+			// 										.map(x->split(" "))
+			// 										.map(x-> new int[]{Integer.valueOf(x[0]), Integer.valueOf(x[1])})
+			// 										.collect();
+			// for(int[] i: l){
+			// 	System.out.println(i);
+			// }
+			String[] lines = input.split("\n"); 
+        int i = 0;
+        int j = 0;
+        for(String line: lines){
+            i = Integer.valueOf(line.split(" ")[0]);
+            j = Integer.valueOf(line.split(" ")[1]);
+            System.out.println(i);
+            System.out.println(j);
+            System.out.println("##############");
+        }
+									
+
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
