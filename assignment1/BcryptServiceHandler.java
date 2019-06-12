7b6b329e35df62d543f29351b62872a6ef66125a
hweng <wenghchn@outlook.com>
2019-06-11 21:46:51 -0400

    a1 basic logic

--- assignment1/BcryptServiceHandler.java
+++ assignment1/BcryptServiceHandler.java
@@ -1,32 +1,41 @@
+import org.apache.thrift.TException;
+import org.mindrot.jbcrypt.BCrypt;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import org.mindrot.jbcrypt.BCrypt;
-
 public class BcryptServiceHandler implements BcryptService.Iface {
-    public List<String> hashPassword(List<String> password, short logRounds) throws IllegalArgument, org.apache.thrift.TException
-    {
-	try {
-	    List<String> ret = new ArrayList<>();
-	    String onePwd = password.get(0);
-	    String oneHash = BCrypt.hashpw(onePwd, BCrypt.gensalt(logRounds));
-	    ret.add(oneHash);
-	    return ret;
-	} catch (Exception e) {
-	    throw new IllegalArgument(e.getMessage());
-	}
+    public List<String> hashPassword(List<String> password, short logRounds) throws IllegalArgument, TException {
+        try {
+            List<String> ret = new ArrayList<>();
+            for (String passwd : password) {
+                String hash = BCrypt.hashpw(passwd, BCrypt.gensalt(logRounds));
+                ret.add(hash);
+            }
+            return ret;
+        } catch (Exception e) {
+            throw new IllegalArgument(e.getMessage());
+        }
     }
 
-    public List<Boolean> checkPassword(List<String> password, List<String> hash) throws IllegalArgument, org.apache.thrift.TException
-    {
-	try {
-	    List<Boolean> ret = new ArrayList<>();
-	    String onePwd = password.get(0);
-	    String oneHash = hash.get(0);
-	    ret.add(BCrypt.checkpw(onePwd, oneHash));
-	    return ret;
-	} catch (Exception e) {
-	    throw new IllegalArgument(e.getMessage());
-	}
+    public List<Boolean> checkPassword(List<String> password, List<String> hash) throws IllegalArgument, TException {
+        try {
+            if (password.size() != hash.size())
+                throw new IllegalArgument();
+            List<Boolean> ret = new ArrayList<>();
+            for (int i = 0; i < password.size(); i++) {
+                ret.add(BCrypt.checkpw(password.get(i), hash.get(i)));
+            }
+            return ret;
+        } catch (Exception e) {
+            throw new IllegalArgument(e.getMessage());
+        }
     }
+
+    @Override
+    public void addBE(String BEhost, short BEport) throws IllegalArgument, TException {
+
+    }
+
+
 }
--- assignment1/Client.java
+++ assignment1/Client.java
@@ -1,47 +1,50 @@
-import java.util.List;
-import java.util.ArrayList;
-
 import org.apache.thrift.TException;
-import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.protocol.TBinaryProtocol;
-import org.apache.thrift.transport.TTransport;
-import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TFramedTransport;
-import org.apache.thrift.transport.TTransportFactory;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Client {
-    public static void main(String [] args) {
-	if (args.length != 3) {
-	    System.err.println("Usage: java Client FE_host FE_port password");
-	    System.exit(-1);
-	}
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.err.println("Usage: java Client FE_host FE_port password");
+            System.exit(-1);
+        }
 
-	try {
-	    TSocket sock = new TSocket(args[0], Integer.parseInt(args[1]));
-	    TTransport transport = new TFramedTransport(sock);
-	    TProtocol protocol = new TBinaryProtocol(transport);
-	    BcryptService.Client client = new BcryptService.Client(protocol);
-	    transport.open();
+        try {
+            TSocket sock = new TSocket(args[0], Integer.parseInt(args[1]));
+            TTransport transport = new TFramedTransport(sock);
+            TProtocol protocol = new TBinaryProtocol(transport);
+            BcryptService.Client client = new BcryptService.Client(protocol);
+            transport.open();
 
-	    List<String> password = new ArrayList<>();
-	    password.add(args[2]);
-	    List<String> hash = client.hashPassword(password, (short)10);
-	    System.out.println("Password: " + password.get(0));
-	    System.out.println("Hash: " + hash.get(0));
-	    System.out.println("Positive check: " + client.checkPassword(password, hash));
-	    hash.set(0, "$2a$14$reBHJvwbb0UWqJHLyPTVF.6Ld5sFRirZx/bXMeMmeurJledKYdZmG");
-	    System.out.println("Negative check: " + client.checkPassword(password, hash));
-	    try {
-		hash.set(0, "too short");
-		List<Boolean> rets = client.checkPassword(password, hash);
-		System.out.println("Exception check: no exception thrown");
-	    } catch (Exception e) {
-		System.out.println("Exception check: exception thrown");
-	    }
+            List<String> password = new ArrayList<>();
+            password.add("1balabala");
+            password.add("2balabala");
+            List<String> hash = client.hashPassword(password, (short) 10);
 
-	    transport.close();
-	} catch (TException x) {
-	    x.printStackTrace();
-	} 
+            for (int i = 0; i < password.size(); i++) {
+                System.out.println("Password: " + password.get(i));
+                System.out.println("Hash: " + hash.get(i));
+            }
+            System.out.println("Positive check: " + client.checkPassword(password, hash));
+            hash.set(0, "$2a$14$reBHJvwbb0UWqJHLyPTVF.6Ld5sFRirZx/bXMeMmeurJledKYdZmG");
+            System.out.println("Negative check: " + client.checkPassword(password, hash));
+            try {
+                hash.set(0, "too short");
+                List<Boolean> rets = client.checkPassword(password, hash);
+                System.out.println("Exception check: no exception thrown");
+            } catch (Exception e) {
+                System.out.println("Exception check: exception thrown");
+            }
+
+            transport.close();
+        } catch (TException x) {
+            x.printStackTrace();
+        }
     }
 }
