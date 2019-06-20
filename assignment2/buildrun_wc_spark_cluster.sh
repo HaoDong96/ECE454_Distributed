257722f8e685fecfbc50dc452af9b7ce326c079b
hweng <wenghchn@outlook.com>
2019-06-20 15:59:05 -0400

    cluster run file

--- assignment2/buildrun_wc_spark_cluster.sh
+++ assignment2/buildrun_wc_spark_cluster.sh
@@ -4,25 +4,27 @@ export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export SCALA_HOME=/usr
 export CLASSPATH=".:/opt/spark-latest/jars/*"
 
+echo -n "Enter Task File:"
+read RUN_FILE
+
 echo --- Deleting
-rm SparkWC.jar
-rm SparkWC*.class
+rm $RUN_FILE.jar
+rm $RUN_FILE*.class
 
 echo --- Compiling
-$SCALA_HOME/bin/scalac -J-Xmx1g SparkWC.scala
+$SCALA_HOME/bin/scalac -J-Xmx1g $RUN_FILE.scala
 if [ $? -ne 0 ]; then
     exit
 fi
 
 echo --- Jarring
-$JAVA_HOME/bin/jar -cf SparkWC.jar SparkWC*.class
+$JAVA_HOME/bin/jar -cf $RUN_FILE.jar $RUN_FILE*.class
 
 echo --- Running
-INPUT=/tmp/smalldata.txt
+INPUT=/a2_inputs/in3.txt
 OUTPUT=/user/${USER}/a2_starter_code_output/
 
 hdfs dfs -rm -R $OUTPUT
-hdfs dfs -copyFromLocal sample_input/smalldata.txt /tmp
-time spark-submit --master yarn --class SparkWC SparkWC.jar $INPUT $OUTPUT
+time spark-submit --master yarn --class $RUN_FILE $RUN_FILE.jar $INPUT $OUTPUT
 
 hdfs dfs -ls $OUTPUT
