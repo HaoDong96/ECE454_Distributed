c516dfc76a79522fb2ae64243ec2f914943a7ed7
hweng <wenghchn@outlook.com>
2019-07-19 17:46:15 -0400

    Preliminary finish for step 5 (delete long print)

--- assignment3/a3_starter/KeyValueHandler.java
+++ assignment3/a3_starter/KeyValueHandler.java
@@ -54,7 +54,6 @@ public class KeyValueHandler implements KeyValueService.Iface {
     @Override
     public void put(String key, String value) throws org.apache.thrift.TException {
         myMap.put(key, value);
-        System.out.println(key + "->" + value + " has been added to primary");
         if (role == Role.PRIMARY && siblingNode.isPresent()) {
             replicateCore(key, value, primaryOps.addAndGet(1));
         }
@@ -64,13 +63,10 @@ public class KeyValueHandler implements KeyValueService.Iface {
     public void replicate(String key, String value, int primaryOps) throws TException {
         if (backupOpsMap.containsKey(key)) {
             if (primaryOps >= backupOpsMap.get(key)) {
-                System.out.println(key + "->" + value + " has been added to backup (overwrite)");
                 myMap.put(key, value);
                 backupOpsMap.put(key, primaryOps);
             }
-            System.out.println(key + "->" + value + " NOT ADDED (TOO OLD)");
         } else {
-            System.out.println(key + "->" + value + " has been added to backup");
             myMap.put(key, value);
             backupOpsMap.put(key, primaryOps);
         }
