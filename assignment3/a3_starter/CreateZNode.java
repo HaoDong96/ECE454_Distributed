71b8149045dfdb44331b9c85bc92268f1cb146de
hweng <wenghchn@outlook.com>
2019-07-18 23:19:24 -0400

    Add znode

--- assignment3/a3_starter/CreateZNode.java
+++ assignment3/a3_starter/CreateZNode.java
@@ -1,47 +1,37 @@
-import java.io.*;
-import java.util.*;
-
-import org.apache.thrift.*;
-import org.apache.thrift.server.*;
-import org.apache.thrift.transport.*;
-import org.apache.thrift.protocol.*;
-
-import org.apache.zookeeper.*;
-import org.apache.zookeeper.data.*;
-import org.apache.curator.*;
-import org.apache.curator.retry.*;
-import org.apache.curator.framework.*;
-import org.apache.curator.utils.*;
-
-import org.apache.log4j.*;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+import org.apache.curator.utils.ZKPaths;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 public class CreateZNode {
     static Logger log;
 
-    public static void main(String [] args) throws Exception {
-	BasicConfigurator.configure();
-	log = Logger.getLogger(StorageNode.class.getName());
+    public static void main(String[] args) throws Exception {
+        BasicConfigurator.configure();
+        log = Logger.getLogger(StorageNode.class.getName());
 
-	if (args.length != 2) {
-	    System.err.println("Usage: java CreateZKNode zkconnectstring zknode");
-	    System.exit(-1);
-	}
+        if (args.length != 2) {
+            System.err.println("Usage: java CreateZKNode zkconnectstring zknode");
+            System.exit(-1);
+        }
 
-	CuratorFramework curClient =
-	    CuratorFrameworkFactory.builder()
-	    .connectString(args[0])
-	    .retryPolicy(new RetryNTimes(10, 1000))
-	    .connectionTimeoutMs(1000)
-	    .sessionTimeoutMs(10000)
-	    .build();
+        CuratorFramework curClient =
+                CuratorFrameworkFactory.builder()
+                        .connectString(args[0])
+                        .retryPolicy(new RetryNTimes(10, 1000))
+                        .connectionTimeoutMs(1000)
+                        .sessionTimeoutMs(10000)
+                        .build();
 
-	curClient.start();
-	Runtime.getRuntime().addShutdownHook(new Thread() {
-		public void run() {
-		    curClient.close();
-		}
-	    });
+        curClient.start();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                curClient.close();
+            }
+        });
 
-	ZKPaths.mkdirs(curClient.getZookeeperClient().getZooKeeper(), args[1]);
+        ZKPaths.mkdirs(curClient.getZookeeperClient().getZooKeeper(), args[1]);
     }
 }
