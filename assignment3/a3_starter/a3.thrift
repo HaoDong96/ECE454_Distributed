d46adea2a99521366087d87b7c33710729d982f0
hdong <spdonghao@gmail.com>
2019-08-14 11:53:14 -0400

    rm

--- assignment3/a3_starter/a3.thrift
+++
@@ -1,7 +0,0 @@
-service KeyValueService {
-  string get(1: string key);
-  void put(1: string key, 2: string value);
-  void replicate(1: string key, 2: string value, 3: i32 primaryOps);
-  void transfer(1: list<string> keys, 2: list<string> values, 3: i32 primaryOps);
-  void ack(1: i32 ackOps);
-}
